d99bdcdd906f5bc8a8b5ac8df5e4945a6292f424
bvegaus <47031212+bvegaus@users.noreply.github.com>
2020-05-19 15:31:37 +0200

    Update weka_costsensitive.py

--- weka_costsensitive.py
+++ weka_costsensitive.py
@@ -11,7 +11,7 @@ import time
 import os
 
 
-def classification(train, test, validation, num_clases):
+def classification(train, opt, validation, num_clases):
         baseClassifiers_list = ["weka.classifiers.bayes.NaiveBayes", "weka.classifiers.functions.MultilayerPerceptron",
                             "weka.classifiers.functions.SMO","weka.classifiers.lazy.IBk", "weka.classifiers.lazy.KStar", "weka.classifiers.meta.AdaBoostM1",
                             "weka.classifiers.meta.Bagging", "weka.classifiers.meta.LogitBoost", "weka.classifiers.trees.J48", "weka.classifiers.trees.DecisionStump",
@@ -19,7 +19,7 @@ def classification(train, test, validation, num_clases):
                             "weka.classifiers.rules.JRip", "weka.classifiers.functions.Logistic", "weka.classifiers.meta.ClassificationViaRegression", 
                             "weka.classifiers.bayes.BayesNet"]
         results_train = pd.DataFrame()
-        results_test = pd.DataFrame()
+        results_opt = pd.DataFrame()
         results_validation = pd.DataFrame()
 
         cost_matrix_list =  [
@@ -42,10 +42,10 @@ def classification(train, test, validation, num_clases):
             real_train.append(train.get_instance(i).values[(train.num_attributes-1)])
         results_train['real'] = real_train
 
-        real_test = [] # the real label of the dataset
-        for i in range(test.num_instances):
-            real_test.append(test.get_instance(i).values[(test.num_attributes-1)])
-        results_test['real'] = real_test
+        real_opt = [] # the real label of the dataset
+        for i in range(opt.num_instances):
+            real_opt.append(opt.get_instance(i).values[(opt.num_attributes-1)])
+        results_opt['real'] = real_opt
 
         real_validation = [] # the real label of the dataset
         for i in range(validation.num_instances):
@@ -65,7 +65,7 @@ def classification(train, test, validation, num_clases):
         
         
             predicted_data_train = None
-            predicted_data_test = None
+            predicted_data_opt = None
             predicted_data_validation = None
 
             evaluation = Evaluation(train)
@@ -81,7 +81,7 @@ def classification(train, test, validation, num_clases):
 
             pred_train = addcls.filter(train)
 
-            pred_test = addcls.filter(test)
+            pred_opt = addcls.filter(opt)
 
             pred_validation = addcls.filter(validation)
 
@@ -94,10 +94,10 @@ def classification(train, test, validation, num_clases):
                 predicted_data_train.add_instance(pred_train.get_instance(n))
 
 
-            if predicted_data_test is None:
-                predicted_data_test = Instances.template_instances(pred_test, 0)
-            for n in range(pred_test.num_instances):
-                predicted_data_test.add_instance(pred_test.get_instance(n))
+            if predicted_data_opt is None:
+                predicted_data_opt = Instances.template_instances(pred_opt, 0)
+            for n in range(pred_opt.num_instances):
+                predicted_data_opt.add_instance(pred_opt.get_instance(n))
 
             if predicted_data_validation is None:
                 predicted_data_validation = Instances.template_instances(pred_validation, 0)
@@ -105,25 +105,25 @@ def classification(train, test, validation, num_clases):
                 predicted_data_validation.add_instance(pred_validation.get_instance(n))
 
             preds_train = [] #labels predicted for the classifer trained in the iteration
-            preds_test = []
+            preds_opt = []
             preds_validation = []
     
             
             for i in range(predicted_data_train.num_instances):
                 preds_train.append(predicted_data_train.get_instance(i).values[(predicted_data_train.num_attributes-1)])
 
-            for i in range(predicted_data_test.num_instances):
-                preds_test.append(predicted_data_test.get_instance(i).values[(predicted_data_test.num_attributes-1)])        
+            for i in range(predicted_data_opt.num_instances):
+                preds_opt.append(predicted_data_opt.get_instance(i).values[(predicted_data_opt.num_attributes-1)])        
 
             for i in range(predicted_data_validation.num_instances):
                 preds_validation.append(predicted_data_validation.get_instance(i).values[(predicted_data_validation.num_attributes-1)])     
 
             results_train[column] = preds_train
-            results_test[column] = preds_test
+            results_opt[column] = preds_opt
             results_validation[column] = preds_validation
 
             num = num+1
-        return results_train, results_test, results_validation
+        return results_train, results_opt, results_validation
 
 
 def dataset_classifiers(dataset, iteration):
@@ -135,45 +135,45 @@ def dataset_classifiers(dataset, iteration):
     loader = Loader("weka.core.converters.ArffLoader")
     train = loader.load_file('./data_weka/discretized-10bins/'+ dataset+ "/"+np.str(iteration)+'.arff')
     train.class_is_last()
-    test = loader.load_file('./data_weka/discretized-10bins/'+ dataset+ "/"+np.str(iteration+5)+'.arff')
-    test.class_is_last()
+    opt = loader.load_file('./data_weka/discretized-10bins/'+ dataset+ "/"+np.str(iteration+5)+'.arff')
+    opt.class_is_last()
     validation = loader.load_file('./data_weka/discretized-10bins/' + dataset + "/" + np.str(iteration+10) + '.arff')
     validation.class_is_last()
 
     num_classes = train.class_attribute.num_values
     print("Numero de clases:    ", num_classes)
 
-    results_train, results_test, results_validation  = classification(train, test, validation, num_classes)
+    results_train, results_opt, results_validation  = classification(train, opt, validation, num_classes)
             
 
 
     train_name = "./predictions/"+dataset+"/" +np.str(iteration) +".csv"
-    test_name = "./predictions/"+dataset+"/" +np.str(iteration+5)+".csv"
+    opt_name = "./predictions/"+dataset+"/" +np.str(iteration+5)+".csv"
     validation_name = "./predictions/"+dataset+"/" +np.str(iteration+10)+".csv"
 
     results_train.to_csv(train_name)
-    results_test.to_csv(test_name)
+    results_opt.to_csv(opt_name)
     results_validation.to_csv(validation_name)
 
     l_train = results_train.real
-    l_test = results_test.real
+    l_opt = results_opt.real
     l_validation = results_validation.real
     accuracy_train_cv = []
-    accuracy_test_cv = []
+    accuracy_opt_cv = []
     accuracy_validation_cv = []
     for classifier in range(18):
         acc_train = 0
-        acc_test = 0
+        acc_opt = 0
         acc_val = 0
         for index_row in range(results_train.shape[0]):
             if l_train[index_row] == results_train.iloc[index_row, classifier+1]:
                 acc_train = acc_train + 1
         acc_train = acc_train / results_train.shape[0]
 
-        for index_row in range(results_test.shape[0]):
-            if l_test[index_row] == results_test.iloc[index_row, classifier+1]:
-                acc_test = acc_test + 1
-        acc_test = acc_test / results_test.shape[0]
+        for index_row in range(results_opt.shape[0]):
+            if l_opt[index_row] == results_opt.iloc[index_row, classifier+1]:
+                acc_opt = acc_opt + 1
+        acc_opt = acc_opt / results_opt.shape[0]
             
         for index_row in range(results_validation.shape[0]):
             if l_validation[index_row] == results_validation.iloc[index_row, classifier+1]:
@@ -181,11 +181,11 @@ def dataset_classifiers(dataset, iteration):
         acc_val = acc_val / results_validation.shape[0]
 
         accuracy_train_cv.append(acc_train*100)
-        accuracy_test_cv.append(acc_test*100)
+        accuracy_opt_cv.append(acc_opt*100)
         accuracy_validation_cv.append(acc_val*100)   
     
     print("Accuracy train:      ",np.mean(accuracy_train_cv))
-    print("Accuracy test:      ",np.mean(accuracy_test_cv))
+    print("Accuracy opt:      ",np.mean(accuracy_opt_cv))
     print("Accuracy validation:      ",np.mean(accuracy_validation_cv))
 
 
